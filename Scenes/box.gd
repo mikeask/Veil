d5f6e6fef895042419eed21ff049d500984406df
taciobn <taciobarreto@hotmail.com>
2018-10-21 18:47:18 -0300

    agora sim

--- Scenes/box.gd
+++ Scenes/box.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
-var gravidade = 500
+var gravidade = 200
 
-var vel_linear = Vector2(0,0)
+var vel_linear = Vector2(1000,0)
 const normal = Vector2(0,-1)
 
 
@@ -10,7 +10,7 @@ func _ready():
 	pass
   
 func _process(delta):
-	vel_linear.y += gravidade*delta*1000
+	vel_linear.y += gravidade*10
 	move_and_slide(vel_linear, normal)
 	
 	vel_linear = Vector2(0,0)
