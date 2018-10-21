d5d4403d5b8678ee61be048c5b5ac6658678ece8
mikeask <31136739+mikeask@users.noreply.github.com>
2018-10-21 16:08:59 -0300

    Credits junto da FinalScene

--- Scenes/CreditsTitle.gd
+++ Scenes/CreditsTitle.gd
@@ -1,11 +1,30 @@
-extends RigidBody2D
+extends Node2D
 
-
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var playing = false
 func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
 	
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _process(delta):
+    
+	if Input.is_action_just_pressed("ui_select") || Input.is_action_just_pressed("ui_accept"):
+		$EscHint.show()
+	
+	if(Input.is_action_just_pressed("ui_cancel")):
+		get_tree().change_scene("res://Scenes/MainMenu.tscn")
+		pass
+	pass
+
+
+func initCredits():
+	$CreditsAnimation.play()
+	pass
+	
+func finishAnimation():
+	get_tree().change_scene("res://Scenes/MainMenu.tscn")
+	pass
