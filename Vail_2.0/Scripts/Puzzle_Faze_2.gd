1e0ef55c7ebe91e6db1a7f354ea2a2391919ec38
taciobn <32937170+taciobn@users.noreply.github.com>
2020-12-21 21:05:08 -0300

    animação do buton

--- Vail_2.0/Scripts/Puzzle_Faze_2.gd
+++ Vail_2.0/Scripts/Puzzle_Faze_2.gd
@@ -1,7 +1,18 @@
 extends Node2D
 
 
+var current_animation = ""
+var animation = ""
+
 func _ready():
 	pass
 func _process(delta):
 	$Porta.isOpen = $buton.ativado
+	
+	if $buton.ativado:
+		current_animation = "DownAnimation"
+	else :
+		current_animation = "UpAnimation"
+	if current_animation != animation:
+		$buton.get_node("AnimationPlayer").play(current_animation)
+		animation = current_animation
